070d9bb55f97a8e118eb3cc470d2d02e609ffa6f
cipriand <ciprian.draghici@webfactory-world.de>
2021-04-09 12:39:36 +0300

    init repo

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <div className="inline-code container"/>
+        </div>
+    );
+}
+
+export default App;
