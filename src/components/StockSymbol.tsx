016fd4e7b69849ff39d9ac243cd47a0638a965c9
cipriand <ciprian.draghici@webfactory-world.de>
2021-04-11 05:15:18 +0300

    use react-virtualize-select to improve symbols search and selection actions

--- src/components/StockSymbol.tsx
+++ src/components/StockSymbol.tsx
@@ -1,14 +1,17 @@
 import React, {useEffect, useState} from "react";
-import Dropdown, {Option} from 'react-dropdown';
 import {ChartService} from "../services/chart.service";
+import VirtualizedSelect from "react-virtualized-select";
+// @ts-ignore
+import createfilteroptions from "react-select-fast-filter-options";
+import {Option, Options} from "react-select";
 
 interface StockSymbolProps {
     handleSymbolChange: (value: string) => void;
 }
 
 const StockSymbol: React.FC<StockSymbolProps> = (props) => {
-    const [selectedSymbol, setSelectedSymbol] = useState<string>();
-    const [symbolOptions, setSymbolOptions] = useState<string[]>([]);
+    const [selectedSymbol, setSelectedSymbol] = useState<Option>();
+    const [symbolOptions, setSymbolOptions] = useState<Option[]>([]);
 
     useEffect(() => {
         getDataAsync();
@@ -18,25 +21,30 @@ const StockSymbol: React.FC<StockSymbolProps> = (props) => {
         const chartService: ChartService = new ChartService();
 
         const data = await chartService.getSymbols() || [];
-        const dropdownOptions: string[] = [];
+        const dropdownOptions: Option[] = [];
 
         for(const stockSymbol of data) {
-            dropdownOptions.push(stockSymbol.symbol)
+            dropdownOptions.push({
+                label: stockSymbol.symbol,
+                value: stockSymbol.symbol
+            });
         }
 
         setSymbolOptions(dropdownOptions);
         if(dropdownOptions.length > 0) {
-            onChangeSymbol({label: dropdownOptions[0], value: dropdownOptions[0]})
+            onChangeSymbol(dropdownOptions[0]);
         }
     }
 
-    const onChangeSymbol = (option: Option) => {
-        setSelectedSymbol(option.value);
-        props.handleSymbolChange(option.value);
+    const onChangeSymbol = (option: Option | null) => {
+        if(!option) { return; }
+
+        setSelectedSymbol(option);
+        props.handleSymbolChange(option.value as string);
     }
 
     return (
-        <Dropdown options={symbolOptions} onChange={onChangeSymbol} value={selectedSymbol} placeholder="Select a symbol" />
+        <VirtualizedSelect options={symbolOptions} filterOptions={createfilteroptions({options: symbolOptions})} value={selectedSymbol} onChange={onChangeSymbol} clearable={false} placeholder="Select a symbol" />
     )
 }
 
